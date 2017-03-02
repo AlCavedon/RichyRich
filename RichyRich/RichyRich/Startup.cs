1304c2ff9b6e71030fd63a603c5a8bf8b5bc7ce6
alcavedon <alcavedon@gmail.com>
2017-03-02 15:48:34 -0500

    new project

---
+++ RichyRich/RichyRich/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RichyRich.Startup))]
+namespace RichyRich
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
